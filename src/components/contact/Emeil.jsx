14a7246da1f8acfd9b39e059bba47df26b3bbb2c
PeterDMP98 <peter.dmp.ca@gmail.com>
2023-05-16 17:31:31 -0500

    fomulario cambio

--- src/components/contact/Emeil.jsx
+++ src/components/contact/Emeil.jsx
@@ -4,22 +4,22 @@ const Emeil = () => {
   return (
     <>
     
-    <form className='form__contact' method='POST' netlify>
+    <form className='form__contact' name='contact' method='POST' data-netlify="true">
 
-        <div className='form__name'>
+        
             <label className='form__label' htmlFor="name">Name</label>
             <input className='form__input' type="text" id='name' name='name' placeholder='Name'/>
-        </div>
+        <br />
 
-        <div className='form__email'>
+        
             <label className='form__label' htmlFor="email">Email</label>
             <input className='form__input' type="email" id='email' name='email' placeholder='Email' />
-        </div>
+        <br />
 
-        <div className='form__message'>
+        
             <label className='form__label' htmlFor="message">Message</label>
             <textarea className='form__text' name="message" id="message" cols="30" rows="10" placeholder='Message'></textarea>
-        </div>
+        <br />
 
         
         <button className='fomr__btn' type="submit">Enviar</button>
