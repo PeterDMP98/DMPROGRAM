8a95bb7a3d2c10cb79e20203a9ff0c5998e12b93
PeterDMP98 <peter.dmp.ca@gmail.com>
2023-05-16 16:53:17 -0500

    fomulario methode

--- src/components/contact/Emeil.jsx
+++ src/components/contact/Emeil.jsx
@@ -2,23 +2,25 @@ import React from 'react'
 
 const Emeil = () => {
   return (
-    <form className='form__contact' name='form__contact' netlify>
-        
+    <form className='form__contact' name='form__contact' method='POST' netlify>
+
+        <div className='form__name'>
             <label className='form__label' htmlFor="name">Name</label>
             <input className='form__input' type="text" id='name' name='name' placeholder='Name'/>
-        <br />
+        </div>
 
-        
+        <div className='form__email'>
             <label className='form__label' htmlFor="email">Email</label>
             <input className='form__input' type="email" id='email' name='email' placeholder='Email' />
-        <br />
+        </div>
 
-        
+        <div className='form__message'>
             <label className='form__label' htmlFor="message">Message</label>
             <textarea className='form__text' name="message" id="message" cols="30" rows="10" placeholder='Message'></textarea>
-        <br />
+        </div>
 
-        <button className='fomr__btn' type="submit" value="Enviar" />
+        
+        <button className='fomr__btn' type="submit">Enviar</button>
 
     </form>
   )
