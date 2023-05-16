22aa332d625dfcfea4c0e0c671af2e22966ca320
PeterDMP98 <peter.dmp.ca@gmail.com>
2023-05-16 16:38:49 -0500

    fomulario div eliminados

--- src/components/contact/Emeil.jsx
+++ src/components/contact/Emeil.jsx
@@ -4,20 +4,20 @@ const Emeil = () => {
   return (
     <form className='form__contact' name='form__contact' netlify>
 
-        <div className='form__name'>
+        
             <label className='form__label' htmlFor="name">Name</label>
             <input className='form__input' type="text" id='name' name='name' placeholder='Name'/>
-        </div>
+        <br />
 
-        <div className='form__email'>
+        
             <label className='form__label' htmlFor="email">Email</label>
             <input className='form__input' type="email" id='email' name='email' placeholder='Email' />
-        </div>
+        <br />
 
-        <div className='form__message'>
+        
             <label className='form__label' htmlFor="message">Message</label>
             <textarea className='form__text' name="message" id="message" cols="30" rows="10" placeholder='Message'></textarea>
-        </div>
+        <br />
 
         <input className='fomr__btn' type="submit" value="Enviar" />
 
