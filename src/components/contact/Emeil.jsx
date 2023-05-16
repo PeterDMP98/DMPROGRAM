e238a114bc467c903569c3d68477cf1b98278334
PeterDMP98 <peter.dmp.ca@gmail.com>
2023-05-16 16:35:01 -0500

    fomulario

--- src/components/contact/Emeil.jsx
+++ src/components/contact/Emeil.jsx
@@ -2,26 +2,24 @@ import React from 'react'
 
 const Emeil = () => {
   return (
-    <form className='form__contact'>
-        
-        <h2 className='form__title'>Contact</h2>
+    <form className='form__contact' name='form__contact' netlify>
 
         <div className='form__name'>
             <label className='form__label' htmlFor="name">Name</label>
-            <input className='form__input' type="text" id='name' placeholder='Name'/>
+            <input className='form__input' type="text" id='name' name='name' placeholder='Name'/>
         </div>
 
         <div className='form__email'>
             <label className='form__label' htmlFor="email">Email</label>
-            <input className='form__input' type="email" id='email' placeholder='Email' />
+            <input className='form__input' type="email" id='email' name='email' placeholder='Email' />
         </div>
 
         <div className='form__message'>
             <label className='form__label' htmlFor="message">Message</label>
-            <textarea className='form__text' name="message" id="message" cols="30" rows="5" placeholder='Message'></textarea>
+            <textarea className='form__text' name="message" id="message" cols="30" rows="10" placeholder='Message'></textarea>
         </div>
 
-        <input className='fomr__btn' type="sutmit" value="Enviar" />
+        <input className='fomr__btn' type="submit" value="Enviar" />
 
     </form>
   )
