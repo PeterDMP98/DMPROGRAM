d1c0d028718a77c69f5008c111fae0951267b339
PeterDMP98 <peter.dmp.ca@gmail.com>
2023-05-16 18:24:13 -0500

    fomulario configuracion

--- src/components/contact/Emeil.jsx
+++ src/components/contact/Emeil.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Emeil = () => {
+
+    const navigate = useNavigate()
+
   return (
     <form className='form__contact' action="https://formsubmit.co/peter.dmp.ca@gmail.com" method='POST'>
 
         <div className='form__name'>
             <label className='form__label' htmlFor="name">Name</label>
             <input className='form__input' type="text" id='name' name='name' placeholder='Name'/>
+        </div>
+
+        <div className='form__subject'>
+            <label className='form__label' htmlFor="subject">Mail Subject</label>
+            <input className='form__input' type="text" id='subject' name='_subject' placeholder='Mail Subject'/>
         </div>
 
         <div className='form__email'>
@@ -19,8 +28,15 @@ const Emeil = () => {
             <textarea className='form__text' name="message" id="message" cols="30" rows="10" placeholder='Message'></textarea>
         </div>
 
+
+        {/* confifuracion de form */}
+
+        <input type="hidden" name='_next' value="http://localhost:2000/#/" />
+        <input type="hidden" name='_captcha' value='false' />
+        <input type="hidden" name='_template' value="box"></input>
         
         <button className='fomr__btn' type="submit">Enviar</button>
+
 
     </form>
   )
