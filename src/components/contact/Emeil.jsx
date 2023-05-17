745c5680112e6656354732148dc81acec042a55b
PeterDMP98 <peter.dmp.ca@gmail.com>
2023-05-16 21:49:04 -0500

    css Contact

--- src/components/contact/Emeil.jsx
+++ src/components/contact/Emeil.jsx
@@ -1,45 +1,44 @@
-import React from 'react'
+import './styles/email.css'
 import { useNavigate } from 'react-router-dom'
 
 const Emeil = () => {
 
-    const navigate = useNavigate()
+    return (
+        <form className='form__contact' action="https://formsubmit.co/peter.dmp.ca@gmail.com" method='POST'>
 
-  return (
-    <form className='form__contact' action="https://formsubmit.co/peter.dmp.ca@gmail.com" method='POST'>
+            <div className='form__name content-info'>
+                <label className='form__label' htmlFor="name">Name</label>
+                <input className='form__input' type="text" id='name' name='name' placeholder='Name' />
+            </div>
 
-        <div className='form__name'>
-            <label className='form__label' htmlFor="name">Name</label>
-            <input className='form__input' type="text" id='name' name='name' placeholder='Name'/>
-        </div>
+            <div className='form__subject content-info'>
+                <label className='form__label' htmlFor="subject">Mail Subject</label>
+                <input className='form__input' type="text" id='subject' name='_subject' placeholder='Mail Subject' />
+            </div>
 
-        <div className='form__subject'>
-            <label className='form__label' htmlFor="subject">Mail Subject</label>
-            <input className='form__input' type="text" id='subject' name='_subject' placeholder='Mail Subject'/>
-        </div>
+            <div className='form__email content-info'>
+                <label className='form__label' htmlFor="email">Email</label>
+                <input className='form__input' type="email" id='email' name='email' placeholder='Email' />
+            </div>
 
-        <div className='form__email'>
-            <label className='form__label' htmlFor="email">Email</label>
-            <input className='form__input' type="email" id='email' name='email' placeholder='Email' />
-        </div>
-
-        <div className='form__message'>
-            <label className='form__label' htmlFor="message">Message</label>
-            <textarea className='form__text' name="message" id="message" cols="30" rows="10" placeholder='Message'></textarea>
-        </div>
+            <div className='form__message content-info'>
+                <label className='form__label' htmlFor="message">Message</label>
+                <textarea className='form__text' name="message" id="message" placeholder='Message'></textarea>
+            </div>
 
 
-        {/* confifuracion de form */}
+            {/* confifuracion de form */}
 
-        <input type="hidden" name='_next' value="http://localhost:2000/#/" />
-        <input type="hidden" name='_captcha' value='false' />
-        <input type="hidden" name='_template' value="box"></input>
-        
-        <button className='fomr__btn' type="submit">Enviar</button>
+            <input type="hidden" name='_next' value="http://localhost:2000/#/" />
+            <input type="hidden" name='_captcha' value='false' />
+            <input type="hidden" name='_template' value="box"></input>
+            
+
+            <button className='form__btn' type="submit">Enviar</button>
 
 
-    </form>
-  )
+        </form>
+    )
 }
 
 export default Emeil
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,8 +4,20 @@ import '../components/contact/styles/contact.css'
 const Contact = () => {
   return (
     <div className='contact'>
-        <h1>Let's talk businees</h1>
-        <Emeil/>
+
+      <h1 className='contact__title'>Let's talk businees</h1>
+
+      <div class="contact__link-phone">
+        <i class="fa-solid fa-square-phone"></i>
+        <a className='contact__phone' href="tel:+573043583617" target="_blank">numero de teleono</a>
+      </div>
+
+      <div class="contact__link-whasapp">
+        <i class="fa-brands fa-square-whatsapp"></i>
+        <a className='contact__whatsapp' href="https://wa.me/573043583617" target="_blank">whatsapp</a>
+      </div>
+
+      <Emeil />
     </div>
   )
 }
